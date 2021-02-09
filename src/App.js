dbec5a06941f846b4c931eabbad4f4248e6115f3
tlswltn <tlswltn0124@nate,com>
2021-02-09 14:01:35 +0900

    two

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+          Learn React ㅋㅋㅋㅋㅎㅎㅋㅋㅋㅋㅋㅋㅋㅋㅋ
         </a>
       </header>
     </div>
